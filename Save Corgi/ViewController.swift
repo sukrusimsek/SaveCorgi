2adc26b652770f1609ec7eafee6aff192b66409a
sukrusimsek <sukrusimsekll@gmail.com>
2024-04-14 17:57:38 +0300

    first commit

---
+++ Save Corgi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Save Corgi
+//
+//  Created by Şükrü Şimşek on 14.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
